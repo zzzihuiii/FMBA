f5aef96b3ce07e0b8dabb12458d7c47438f36a96
zzzihuiii <99530085+zzzihuiii@users.noreply.github.com>
2022-02-12 16:34:08 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from flask import Flask 
+
+
+# In[2]:
+
+
+app = Flask(__name__)
+
+
+# In[3]:
+
+
+from flask import request, render_template
+import joblib
+
+@app.route("/", methods=["Get","POST"])
+def index():
+    if request.method == "POST":
+        rates = request.form.get("rates")
+        print(rates)
+        model = joblib.load("DBS")
+        pred = model.predict([[float(rates)]])
+        print(pred)
+        s = "The predicted DBS share price is " + str(pred[0][0])
+        return(render_template("index.html", result=s))
+    else:
+        return(render_template("index.html",result="2"))
+
+
+# In[ ]:
+
+
+if __name__ == "__main__":
+    app.run()
+
+
+# In[ ]:
+
+
+
+
